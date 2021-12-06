84a6ed0cb2e00ae7c61b45d94dec8f4984914c62
MargaritaJarovaja <nord.ferro@gmail.com>
2021-12-06 20:52:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+//http://localhost:3000/form  (path)
+
+const express = require("express");
+const app = express();
+app.use(express.static("publik"));
+app.listen("3000");
+console.log("Kör servern på localhost:3000");
+
+app.get("/form", (req, res) => {
+    res.sendFile(__dirname + "/form.html");
+});
+
+
+const fs = require("fs");
+app.get("/hamta-json", (req, res) => {
+    console.log("Mottog förfrågan med XMLHttpRequest från klienten");
+    fs.readFile("messages.json", function(err, data) {
+        if (err) throw err; 
+        res.send(data);
+    });
+});
+
+
+app.use(express.urlencoded({extended: true}));  
+app.post("/skriv-fran-mall", (req, res) => {
+ 
+    fs.readFile("form.html", function(err, data){
+        fs.readFile("messages.json", function(err, postJson) {
+        
+            let falt = JSON.parse(postJson);
+            let date = new Date;
+            let precisDate = date.toISOString();
+            precisDate = precisDate.substring(0, 16);
+            
+
+            let nyUser = {    // skapa ny user
+                date: precisDate +'<br>',
+                namn: req.body.name +'<br>',
+                efternamn: req.body.surname +'<br>',
+                email: req.body.email +'<br>',
+                meddelande: req.body.meddelande +'<br>',
+                string: '------------------------------'
+            };
+
+            console.log(nyUser);
+            falt.push(nyUser);    // lägg till nya user
+            
+            let nyJson = JSON.stringify(falt, null, 2);
+            fs.writeFile("messages.json", nyJson, (err) => {
+                if (err) throw err;
+            });
+            
+            let output = "";
+            for (messages in falt) {
+                for (attribut in messages) {
+                    output += messages[attribut] + " ";
+                }
+                output += "<br>";
+            }
+            let html = data.toString().replace(output);
+            res.send(html);
+        });
+    });
+});
---
+++ publik/javascript/klientskript.js
@@ -0,0 +1,24 @@
+
+let jsonText = '{"date": "2021-12-06T19:06<br>","namn": "Margarita<br>","efternamn": "Jarovaja<br>","email": "nord.ferro@gmail.com<br>","meddelande": "Poka!<br>"}'; 
+let object = JSON.parse(jsonText); 
+ 
+let hamtaData = function() {
+    let forfragan = new XMLHttpRequest();  
+    forfragan.open("GET", "/hamta-json"); 
+    forfragan.onload = function() {                  
+        data = JSON.parse(this.response);  
+        console.log(data);
+        for (let i = 0; i < data.length; i++) { 
+            for (attribut in data[i]) {
+                document.getElementById("output").innerHTML += data[i][attribut] + " ";
+            };
+            document.getElementById("output").innerHTML += "<br>";
+        };
+    };
+    forfragan.send();                      
+};
+window.onload = function() {
+    
+    hamtaData();
+
+};
